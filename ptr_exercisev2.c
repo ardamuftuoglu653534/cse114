519c7a1e6d5f8f6eea1145e9d6c2c9dcefb4cd49
Arda Müftüoğlu <120189283+ardamuftuoglu653534@users.noreply.github.com>
2023-12-16 16:09:41 +0300

    Create ptr_exercisev2.c

---
+++ ptr_exercisev2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// pointer kullanarak aranılan elemanın kaçıncı pozisyonda olduğunu yazdırma
+void aranilan_eleman(int *p,int eleman);
+
+int main() {
+   int A[6]={10,48,13,28,96,73};
+   int eleman;
+   printf("Lütfen aradığınız lemanı girin: ");
+   scanf("%d",&eleman);
+    aranilan_eleman(A,eleman);
+    // printf("Hello, World!\n");
+    return 0;
+}
+void aranilan_eleman(int *p,int eleman){
+    int i=0;
+    for(i=0;i<sizeof(p)/sizeof(p[0]);i++){
+        p+i;
+        i++;
+        if(*(p+i)==eleman) printf("aranilan eleman i.konumdadır:%d",i+1);
+        else
+            printf("Aradıgınız eleman bulunmamaktadır");
+
+    }
