65d93913dc7c8fd0eaa83145274891ba30213dec
Arda Müftüoğlu <120189283+ardamuftuoglu653534@users.noreply.github.com>
2023-12-27 23:29:22 +0300

    Create lessthannumrecursive.c

---
+++ lessthannumrecursive.c
@@ -0,0 +1,24 @@
+
+// bir arrayda kendisinden küçük olan sayıları yazdıran rec. fonk.
+#include <stdio.h>
+int lessThanNum(int A[],int num,int i);
+int main()
+{
+    int num;
+    int i=0;
+    printf("Please Enter num: ");
+    scanf("%d",&num);
+    int A[]={1,3,5,49,16,73,7,88,97};
+    printf("%d",lessThanNum(A,num,i));
+
+
+    return 0;
+}
+int lessThanNum(int A[],int num,int i){
+
+    if (A[i]>=num) return 0;
+    printf("%d ",A[i]);
+    return lessThanNum(A,num,i+1);
+
+
+}
